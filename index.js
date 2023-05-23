564fd592179bfdf0afbca3a13109d27a00ec37e9
annzhzho@gmail.com <annzhzho@gmail.com>
2023-05-23 20:15:17 +0300

    add case function

---
+++ index.js
@@ -0,0 +1,22 @@
+const userNameInput = document.querySelector("#user-name");
+const userLinkInput = document.querySelector("#user-link");
+const userTextInput = document.querySelector("#user-text");
+const button = document.querySelector(".button");
+
+const user = {
+  name: userNameInput.value,
+  avatar: userLinkInput.value,
+  text: userTextInput.textContent,
+};
+
+//Изменение регистра имени
+function modifyUserName() {
+  if (!user.name) return user.name;
+  return user.name[0].toUpperCase() + user.name.slice(1).toLowerCase();
+}
+
+console.log(modifyUserName());
+
+let commentName = document.querySelector(".comment__name");
+
+button.addEventListener("click", postComment(), {});
