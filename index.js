052d45de540cefa891a088d54e077b36bf0187f1
annzhzho@gmail.com <annzhzho@gmail.com>
2023-05-28 11:03:14 +0300

    remove comments

--- index.js
+++ index.js
@@ -2,18 +2,14 @@ const userName = document.querySelector("#user-name");
 const userLink = document.querySelector("#user-link");
 const userText = document.querySelector("#user-text");
 
-//Изменение регистра имени
-function modifyUserName() {
-  if (!userName.value) {
-    return userName.value;
+function modifyUserName(str) {
+  if (!str) {
+    return str;
   } else {
-    return (
-      userName.value[0].toUpperCase() + userName.value.slice(1).toLowerCase()
-    ).trim();
+    return (str[0].toUpperCase() + str.slice(1).toLowerCase()).trim();
   }
 }
 
-//добавление фото
 const commentAvatar = document.querySelector(".comment__avatar");
 
 function showAvatar(src) {
@@ -22,32 +18,11 @@ function showAvatar(src) {
   commentAvatar.append(image);
 }
 
-//антиспам
-
 function checkSpam(str) {
   str = str.replace(/viagra/gi, "***");
   str = str.replace(/XXX/gi, "***");
   return str;
 }
-
-function createNewComment(userName, avatar, comment) {}
-
-/* const createHTML = (userName, link, text) => {
-  const htmlElements = [];
-  htmlElements.push('<div class="comment">');
-
-  let srcSrtring = `<img class="userphoto" src="${link}" alt="userLogo">`;
-  htmlElements.push(srcSrtring);
-  let userNameString = `<strong class='userHeader'>${userName}</strong>`;
-  htmlElements.push(userNameString);
-  let commentString = `<p class='comment-content'>${text}</p>`;
-  htmlElements.push(`${commentString}`);
-  const currentTime = getDate();
-  htmlElements.push(`<div class='posttime'>${currentTime}</div>`);
-  htmlElements.push(`</div>`);
-
-  return htmlElements.join("");
-}; */
 
 const button = document.querySelector(".button");
 const commentName = document.querySelector(".comment__name");
@@ -55,7 +30,6 @@ const commentText = document.querySelector(".comment__text");
 const plugName = "Anonimus";
 const plugAvatar = "./assets/images/avatar_null.jpg";
 
-//чтото надо сделать с кнопкой
 button.addEventListener("click", (event) => {
   event.preventDefault();
 
